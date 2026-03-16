1326714f0d6b38497f7a6ac357400c9dd1b1d8c0
Fernando Junior <168040341+FernandoJR07@users.noreply.github.com>
2026-03-16 19:30:20 -0300

    fix: Ajustes solicitados aplicados com sucesso.

--- src/main/java/com/grupo6/barbearia_api/model/Cargo.java
+++ src/main/java/com/grupo6/barbearia_api/model/Cargo.java
@@ -10,16 +10,14 @@ public class Cargo {
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
 
-    @Column(name = "name", nullable = false, unique = true)
-    private String name;
+    @Column(name = "nome", nullable = false, unique = true)
+    private String nome;
 
     public Cargo() {}
-
-    public Cargo(String name) { this.name = name; }
+    public Cargo(String nome) { this.nome = nome; }
 
     public Long getId() { return id; }
     public void setId(Long id) { this.id = id; }
-
-    public String getName() { return name; }
-    public void setName(String name) { this.name = name; }
+    public String getNome() { return nome; }
+    public void setNome(String nome) { this.nome = nome; }
 }
--- src/main/java/com/grupo6/barbearia_api/model/Usuario.java
+++ src/main/java/com/grupo6/barbearia_api/model/Usuario.java
@@ -13,10 +13,7 @@ public class Usuario {
     private Long id;
 
     @Column(nullable = false, unique = true)
-    private String username;
-
-    @Column(nullable = false)
-    private String password;
+    private String email;
 
     @ManyToMany(fetch = FetchType.EAGER)
     @JoinTable(
@@ -30,10 +27,8 @@ public class Usuario {
 
     public Long getId() { return id; }
     public void setId(Long id) { this.id = id; }
-    public String getUsername() { return username; }
-    public void setUsername(String username) { this.username = username; }
-    public String getPassword() { return password; }
-    public void setPassword(String password) { this.password = password; }
+    public String getEmail() { return email; }
+    public void setEmail(String email) { this.email = email; }
     public Set<Cargo> getCargos() { return cargos; }
     public void setCargos(Set<Cargo> cargos) { this.cargos = cargos; }
 }
